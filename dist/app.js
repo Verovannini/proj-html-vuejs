235337f91b7978c8ba6ec8f054e404f86cc8092e
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-05-12 18:19:35 +0200

    end footer with vue

--- dist/app.js
+++ dist/app.js
@@ -10,8 +10,11 @@
 var app = new Vue({
   el: '#root',
   data: {
+    // Links List Header Menu
     linksHeader: ['Home', 'About', 'Departments', 'Articles', 'Make Appointment'],
-    activeLink: 0
+    activeLink: 0,
+    // Links List Footer Menu
+    linksFooter: ['Cardiology', 'Pediatrics', 'Diabetes Care', 'Pre-natel Care', 'Ultrasound Echocardiogram']
   }
 });
 
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ var app = new Vue(
     {
         el: '#root',
         data: {
+            // Links List Header Menu
             linksHeader: [
                 'Home',
                 'About',
@@ -10,6 +11,15 @@ var app = new Vue(
                 'Make Appointment'
             ],
             activeLink: 0,
+
+            // Links List Footer Menu
+            linksFooter: [
+                'Cardiology',
+                'Pediatrics',
+                'Diabetes Care',
+                'Pre-natel Care',
+                'Ultrasound Echocardiogram'
+            ]
         }
     }
 );
