ca703dbca9bb9a8d5cc2a741b4fabc273568e424
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-05-13 17:05:31 +0200

    correzioni

--- dist/app.js
+++ dist/app.js
@@ -14,9 +14,12 @@ var app = new Vue({
     linksHeader: ['Home', 'About', 'Departments', 'Articles', 'Make Appointment'],
     activeLink: 0,
     // Links List Footer Menu
-    linksFooter: ['Cardiology', 'Pediatrics', 'Diabetes Care', 'Pre-natel Care', 'Ultrasound Echocardiogram']
+    linksFooter: ['Cardiology', 'Pediatrics', 'Diabetes Care', 'Pre-natel Care', 'Ultrasound Echocardiogram'],
+    // Logo images paths
+    logos: ["img/client-logos-1.png", "img/client-logos-2.png", "img/client-logos-3.png", "img/client-logos-4.png"]
   },
   methods: {
+    // Function that scroll at the top of the page
     toTop: function toTop() {
       window.scrollTo({
         top: 0,
--- src/app.js
+++ src/app.js
@@ -19,9 +19,18 @@ var app = new Vue(
                 'Diabetes Care',
                 'Pre-natel Care',
                 'Ultrasound Echocardiogram'
+            ],
+
+            // Logo images paths
+            logos: [
+                "img/client-logos-1.png",
+                "img/client-logos-2.png",
+                "img/client-logos-3.png",
+                "img/client-logos-4.png"
             ]
         },
         methods: {
+            // Function that scroll at the top of the page
             toTop() {
                 window.scrollTo({
                     top: 0,
