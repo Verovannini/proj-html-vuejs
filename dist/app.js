50cbc71287cddcb382e57b27a8f1f1ab1b72c14d
Veronica Vannini <veronica.vanniniv@gmail.com>
2021-05-13 10:24:11 +0200

    added :hover and scroll to top function

--- dist/app.js
+++ dist/app.js
@@ -15,6 +15,15 @@ var app = new Vue({
     activeLink: 0,
     // Links List Footer Menu
     linksFooter: ['Cardiology', 'Pediatrics', 'Diabetes Care', 'Pre-natel Care', 'Ultrasound Echocardiogram']
+  },
+  methods: {
+    toTop: function toTop() {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
   }
 });
 
--- src/app.js
+++ src/app.js
@@ -20,6 +20,15 @@ var app = new Vue(
                 'Pre-natel Care',
                 'Ultrasound Echocardiogram'
             ]
+        },
+        methods: {
+            toTop() {
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }
         }
     }
 );
